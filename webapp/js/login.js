179c20110470ca7e7aba168a86d5dbea2b602f32
k1m0j <kimoji@posteo.de>
2021-05-16 12:52:31 +0200

    add login methods

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,7 +1,13 @@
 function showLoginContent() {
-
+    let loginDiv = document.getElementById("login");
+    let mainContentDiv = document.getElementById("mainContent")
+    loginDiv.style.display = 'block'
+    mainContentDiv.style.display = 'none'
 }
 
 function showLoggedInContent() {
-    
+    let loginDiv = document.getElementById("login");
+    let mainContentDiv = document.getElementById("mainContent")
+    loginDiv.style.display = 'none'
+    mainContentDiv.style.display = 'block'
 }
