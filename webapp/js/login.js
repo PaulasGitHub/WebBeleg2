ddcdccc923ce8c278476e0f4a711433dd70e31f6
k1m0j <kimoji@posteo.de>
2021-05-16 15:35:54 +0200

    refactor login method

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -14,15 +14,21 @@ function login() {
     let givenUsername = document.getElementById('usernameInput').value
     let givenPassword = document.getElementById('passwordInput').value
 
-    users.forEach(function (item) {
-        if (item.name === givenUsername && item.password === givenPassword) {
-            let loginDiv = document.getElementById("login");
-            let mainContentDiv = document.getElementById("mainContent")
-            loginDiv.style.display = 'none'
-            mainContentDiv.style.display = 'block'
-
-            document.getElementById('usernameInput').value = null;
-            document.getElementById('passwordInput').value = null;
+    let userNotFound = true
+    let currentUserIndex = 0
+    while (userNotFound && currentUserIndex < users.length) {
+        let currentUser = users[currentUserIndex]
+        if (currentUser.name === givenUsername && currentUser.password === givenPassword) {
+            document.getElementById("login").style.display = 'none'
+            document.getElementById("mainContent").style.display = 'block'
+            document.getElementById('welcomeHeader').innerHTML = "Welcome " + givenUsername + "!"
+            document.getElementById('errorMessageLogin').innerHTML = ''
+            userNotFound = false
         }
-    });
+        currentUserIndex++
+    }
+    if (userNotFound) document.getElementById('errorMessageLogin').innerHTML = "Login credentials where incorrect! Please try again!"
+    document.getElementById('usernameInput').value = null;
+    document.getElementById('passwordInput').value = null;
+
 }
