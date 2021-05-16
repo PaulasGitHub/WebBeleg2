b74186bb4378f7884ef12a67fb7eae159f2c1b5f
k1m0j <kimoji@posteo.de>
2021-05-16 15:02:01 +0200

    fix login method

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -3,7 +3,7 @@ const users = [
     {name: "normalo", password: "pass321"}
 ]
 
-function showLoginContent() {
+function logout() {
     let loginDiv = document.getElementById("login");
     let mainContentDiv = document.getElementById("mainContent")
     loginDiv.style.display = 'block'
@@ -13,13 +13,16 @@ function showLoginContent() {
 function login() {
     let givenUsername = document.getElementById('usernameInput').value
     let givenPassword = document.getElementById('passwordInput').value
-    
+
     users.forEach(function (item) {
         if (item.name === givenUsername && item.password === givenPassword) {
             let loginDiv = document.getElementById("login");
             let mainContentDiv = document.getElementById("mainContent")
             loginDiv.style.display = 'none'
             mainContentDiv.style.display = 'block'
+
+            document.getElementById('usernameInput').value = null;
+            document.getElementById('passwordInput').value = null;
         }
     });
 }
