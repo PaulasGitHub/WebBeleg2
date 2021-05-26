c63a51ca996851e2ec0145fcd6239820766e50e6
k1m0j <kimoji@posteo.de>
2021-05-26 11:10:45 +0200

    relocate contacts js

--- webapp/js/logic.js
+++ webapp/js/logic.js
@@ -95,116 +95,6 @@ function login() {
     document.getElementById('passwordInput').value = null;
 }
 
-//TODO: Enter = LogInButton-click
-/**
- * Display all contacts depending on the currently logged in role
- */
-function displayAllContacts() {
-    clearContactsView()
-    showAllContactsOnMapAsMarkers()
-    let currentUserRole = loggedInUser.role
-    if (currentUserRole === 'admin') {
-        users.forEach(function (user) {
-            displayContacts(user.contacts)
-        })
-    } else if (currentUserRole === 'user') {
-        users.forEach(function (user) {
-            if (user.name === loggedInUser.name) {
-                displayContacts(user.contacts)
-            } else {
-                displayPublicContactsOnly(user.contacts)
-            }
-        })
-    }
-}
-
-/**
- * Display only contacts from currently logged in user
- */
-function displayOwnContacts() {
-    clearContactsView()
-    showOwnContactsOnMapAsMarkers()
-    displayContacts(loggedInUser.contacts)
-}
-
-/**
- * Display a given contact array
- * @param contacts given contact array
- */
-function displayContacts(contacts) {
-    let contactsDiv = document.getElementById('contactsDiv')
-    contacts.forEach(function (contact) {
-        let contactList = document.createElement('UL')
-        for (let propertyName in contact) {
-            addContactListElement(contactList, contact[propertyName])
-        }
-        contactsDiv.appendChild(contactList)
-    });
-}
-
-/**
- * Display only contacts from a given contact array where the private attribute is set to false
- * @param contacts given contact array
- */
-function displayPublicContactsOnly(contacts) {
-    let contactsDiv = document.getElementById('contactsDiv')
-    contacts.forEach(function (contact) {
-        if (!contact.private) {
-            let contactList = document.createElement('UL')
-            for (let propertyName in contact) {
-                addContactListElement(contactList, contact[propertyName])
-            }
-            contactsDiv.appendChild(contactList)
-        }
-    })
-}
-
-/**
- * Add a single contact attribute to a single contact
- * @param contactList contact where the attribute is added to
- * @param contactAttribute attribute which will be added
- */
-function addContactListElement(contactList, contactAttribute) {
-    let listElement = document.createElement("LI");          // Create a <li> node
-    let listElementText = document.createTextNode(contactAttribute);         // Create a text node
-    listElement.appendChild(listElementText);
-    contactList.appendChild(listElement)
-}
-
-function addContact() {
-    let newContact = {}
-    readContactInput(newContact, "firstNameInputAddForm", 'firstName')
-    readContactInput(newContact, "lastNameInputAddForm", 'lastName')
-    readContactInput(newContact, "streetInputAddForm", 'street')
-    readContactInput(newContact, "numberInputAddForm", 'number')
-    readContactInput(newContact, "zipInputAddForm", 'zip')
-    readContactInput(newContact, "cityInputAddForm", 'city')
-    readContactInput(newContact, "stateInputAddForm", 'state')
-    readContactInput(newContact, "countryInputAddForm", 'country')
-    newContact["private"] = document.getElementById("privateCheckAddForm").checked
-
-    if (loggedInUser.role == "user") {
-        loggedInUser.contacts.push(newContact)
-    } else if (loggedInUser.role == 'admin') {
-
-        if (document.getElementById('ownerSelectAddForm').value == 'self')
-            loggedInUser.contacts.push(newContact)
-        else {
-            let newArray = users.filter(function (user) {
-                return user.name == 'normalo'
-            });
-            newArray[0].contacts.push(newContact)
-        }
-    }
-    displayOwnContacts()
-    displayMainContentScreen()
-}
-
-function readContactInput(newContact, inputID, attributeName) {
-    newContact[attributeName] = document.getElementById(inputID).value
-}
-
-
 /**
  * Display the "change contacts" screen
  */
@@ -223,14 +113,6 @@ function displayAddContactScreen() {
 function displayMainContentScreen() {
     displayElements('mainContent')
     hideElements('changeContacts')
-}
-
-
-/**
- * Remove all contacts from the view
- */
-function clearContactsView() {
-    document.getElementById('contactsDiv').innerHTML = ""
 }
 
 /**
