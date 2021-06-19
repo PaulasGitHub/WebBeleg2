d55020244e3dbf8167797fe744e52a624e798423
k1m0j <kimoji@posteo.de>
2021-06-19 15:16:14 +0200

    add login post method

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,25 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/login', function (req, res) {
-    if (true) {
-        res.status(200)
+    let loginCredential = req.body
+    let validatedCredentials = isLoginCredentialValid(loginCredential)
+    if (validatedCredentials.loginValid) {
+        res.status(200).json(validatedCredentials.user)
     } else {
         res.status(401).send('Unauthorized')
     }
 })
+
+function isLoginCredentialValid(loginCredentials) {
+    let validatedCredentials = null
+    validatedCredentials.loginValid = false
+    users.forEach(function (user) {
+        if (user.name == loginCredentials.name && user.password == loginCredentials.password) {
+            validatedCredentials.loginValid = true;
+            validatedCredentials.user = user
+        }
+    })
+    return validatedCredentials
+}
 
 module.exports = router;
