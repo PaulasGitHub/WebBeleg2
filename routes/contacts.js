79512fe58b24ea428dc8faa527e3ee6a34b6fb21
k1m0j <kimoji@posteo.de>
2021-06-19 16:55:56 +0200

    add crud methods to contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,4 +1,23 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/', function (req, res) {
+    let requestedUserId = req.query.userId
+    res.status(200)
+})
+
+router.post('/', function (req, res) {
+    res.status(201)
+})
+
+router.delete('/:id', function (req, res) {
+    let contactId = req.params.id;
+    res.status(204)
+})
+
+router.post('/:id', function (req, res) {
+    let contactId = req.params.id;
+    res.status(204)
+})
+
 module.exports = router;
