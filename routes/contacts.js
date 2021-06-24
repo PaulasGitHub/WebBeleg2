bbeef54cc4577ecd082ed088eeabb1e5b2ac2ae8
k1m0j <kimoji@posteo.de>
2021-06-24 19:03:45 +0200

    remove hardcoded model

--- routes/contacts.js
+++ routes/contacts.js
@@ -80,61 +80,6 @@ router.put('/:id', function (req, res) {
     })
 })
 
-const contactsNEW = [
-    {
-        id: 1,
-        firstName: 'Maxime',
-        lastName: 'Musterfrau',
-        street: 'Sonnenalle',
-        number: '12',
-        zip: '12345',
-        city: 'Berlin',
-        state: 'Berlin',
-        country: 'Deutschland',
-        owner: 'admina',
-        private: true
-    },
-    {
-        id: 2,
-        firstName: 'Todd',
-        lastName: 'Chavez',
-        street: 'Carl-Herz-Ufer',
-        number: '25',
-        zip: '10961',
-        city: 'Berlin',
-        state: 'Berlin',
-        country: 'Deutschland',
-        owner: 'admina',
-        private: false
-    },
-    {
-        id: 3,
-        firstName: 'Jo',
-        lastName: 'Guenther',
-        street: "Mittenwalder Straße",
-        number: '12',
-        zip: '23433',
-        city: 'Berlin',
-        state: 'Berlin',
-        country: 'Deutschland',
-        owner: 'normalo',
-        private: true
-    },
-    {
-        id: 4,
-        firstName: 'Hans',
-        lastName: 'Herbert',
-        street: 'Pfarrstraße',
-        number: '113',
-        zip: '10961',
-        city: 'Berlin',
-        state: 'Berlin',
-        country: 'Deutschland',
-        owner: 'normalo',
-        private: false
-    }
-]
-
 module.exports = router;
 
 //TODO app.listen(port, () => {
