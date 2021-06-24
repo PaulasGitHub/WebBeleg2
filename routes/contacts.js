6b52646825e85e38780f8881c6c3dd8f020cf496
k1m0j <kimoji@posteo.de>
2021-06-24 18:37:53 +0200

    add delete method

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,7 @@
 let express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 let router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
 
 const url = "mongodb://localhost:27017/";
 const dbName = 'advizDB'
@@ -11,15 +12,11 @@ router.get('/', function (req, res) {
         let requestedUserId = req.query.userId
         MongoClient.connect(url, {useUnifiedTopology: true},
             function (err, client) {
-                if (err) {
-                    throw err
-                }
+                if (err) throw err
                 let db = client.db(dbName);
                 db.collection(contactsCollections).find({owner: requestedUserId}).toArray(
                     function (err, result) {
-                        if (err) {
-                            throw err;
-                        }
+                        if (err) throw err
                         if (result.length == 0) {
                             res.sendStatus(404)
                         } else {
@@ -40,13 +37,10 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     MongoClient.connect(url, {useUnifiedTopology: true},
         function (err, client) {
-            if (err) {
-                throw err
-            }
+            if (err) throw err
             let db = client.db(dbName);
             db.collection(contactsCollections).insertOne(req.body, function (err, result) {
-                if (err)
-                    throw err
+                if (err) throw err
                 else
                     res.sendStatus(201);
                 //TODO db.close oder client.close
@@ -59,7 +53,16 @@ router.post('/', function (req, res) {
 
 router.delete('/:id', function (req, res) {
     let contactId = req.params.id;
-    res.status(204)
+    MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
+        if (err) throw err
+        let db = client.db(dbName);
+        db.collection(contactsCollections).deleteOne({_id: ObjectId(contactId)}, function (err, result) {
+            if (err) throw err
+            console.log(result)
+            res.sendStatus(204)
+            client.close()
+        })
+    })
 })
 
 router.put('/:id', function (req, res) {
